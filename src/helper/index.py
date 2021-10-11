504c9c5262a5623822c8f3bba9cdf324c330bbf7
Eldar Aliiev <bc.zim.green@gmail.com>
2021-10-12 01:07:01 +0300

    S3 trigger lambda fixed

--- src/helper/index.py
+++ src/helper/index.py
@@ -1,71 +1,74 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
 import json
 import boto3
 import cfnresponse
 
-print('Loading function')
-s3 = boto3.resource('s3')
+
+print("Loading function")
+s3 = boto3.client('s3')
+
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-    responseData={}
+    print("Received event:", json.dumps(event, indent=2))
     try:
-        if event['RequestType'] == 'Delete':
-            print("Request Type:",event['RequestType'])
+        print("Request Type:", event['RequestType'])
+        BucketNotificationConfiguration = s3.get_bucket_notification_configuration(
             Bucket=event['ResourceProperties']['Bucket']
-            delete_notification(Bucket)
-            print("Sending response to custom resource after Delete")
-        elif event['RequestType'] == 'Create' or event['RequestType'] == 'Update':
-            print("Request Type:",event['RequestType'])
-            LambdaArn=event['ResourceProperties']['LambdaArn']
-            Bucket=event['ResourceProperties']['Bucket']
-            Prefix=event['ResourceProperties']['Prefix']
-            Suffix=event['ResourceProperties']['Suffix']
-            add_notification(LambdaArn, Bucket, Prefix, Suffix)
-            responseData={'Bucket':Bucket}
-            print("Sending response to custom resource")
-        responseStatus = 'SUCCESS'
+        )
+        BucketNotificationConfiguration.pop('ResponseMetadata')
+        BucketNotificationConfiguration.update({'LambdaFunctionConfigurations': []})
+
+        if event['RequestType'] in ['Update', 'Delete']:
+            BucketNotificationConfiguration['LambdaFunctionConfigurations'] = list(
+                filter(
+                    lambda configuration: configuration.get('Id') != event['PhysicalResourceId'],
+                    BucketNotificationConfiguration['LambdaFunctionConfigurations']
+                )
+            )
+
+        if event['RequestType'] in ['Create', 'Update']:
+            BucketNotificationConfiguration['LambdaFunctionConfigurations'].append({
+                'Id': event.get('PhysicalResourceId', context.aws_request_id),
+                'LambdaFunctionArn': event['ResourceProperties']['LambdaArn'],
+                'Filter': {
+                    'Key': {
+                        'FilterRules': [
+                            {
+                                'Name': 'prefix',
+                                'Value': event['ResourceProperties'].get('Prefix', '')
+                            },
+                            {
+                                'Name': 'suffix',
+                                'Value': event['ResourceProperties'].get('Suffix', '')
+                            },
+                        ]
+                    }
+                },
+                'Events': [
+                    's3:ObjectCreated:*'
+                ]
+            })
+
+        if len(BucketNotificationConfiguration['LambdaFunctionConfigurations']) == 0:
+            BucketNotificationConfiguration.pop('LambdaFunctionConfigurations')
+
+        s3.put_bucket_notification_configuration(
+            Bucket=event['ResourceProperties']['Bucket'],
+            NotificationConfiguration=BucketNotificationConfiguration
+        )
+        responseStatus = cfnresponse.SUCCESS
+        print(event['RequestType'], "request completed....")
     except Exception as e:
-        print('Failed to process:', e)
-        responseStatus = 'FAILED'
-        responseData = {'Failure': 'Something bad happened.'}
+        print("Failed to process:", e)
+        responseStatus = cfnresponse.FAILED
     finally:
-        cfnresponse.send(event, context, responseStatus, responseData)
-
-def add_notification(LambdaArn, Bucket, Prefix, Suffix):
-    s3.BucketNotification(Bucket).put(
-        NotificationConfiguration={
-            'LambdaFunctionConfigurations': [
-                {
-                    'LambdaFunctionArn': LambdaArn,
-                    'Filter': {
-                        'Key': {
-                            'FilterRules': [
-                                {
-                                    'Name': 'prefix',
-                                    'Value': Prefix
-                                },
-                                {
-                                    'Name': 'suffix',
-                                    'Value': Suffix
-                                },
-                            ]
-                        }
-                    },
-                    'Events': [
-                        's3:ObjectCreated:*'
-                    ]
-                }
-            ]
-        }
-    )
-    print("Put request completed....")
-
-def delete_notification(Bucket):
-    s3.BucketNotification(Bucket).put(
-        NotificationConfiguration={}
-    )
-    print("Delete request completed....")
+        print("Sending response to custom resource")
+        cfnresponse.send(
+            event,
+            context,
+            responseStatus,
+            {},
+            event.get('PhysicalResourceId', context.aws_request_id)
+        )
