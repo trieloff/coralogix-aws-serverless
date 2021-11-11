67d43de1cf001c8c1a72fefa36d91f1ea0acae63
Juan Gimenez <juan@coralogix.com>
2021-11-11 09:17:29 -0300

    Added CloudTrail Encrypt Test

--- src/auto-posture-evaluator/testers/cloudtrail_tester.py
+++ src/auto-posture-evaluator/testers/cloudtrail_tester.py
@@ -1,10 +1,7 @@
 import time
 import boto3
-import re
-import ipaddress
-import botocore.exceptions
 import interfaces
-import json
+
 
 class CloudtrailTester(interfaces.TesterInterface):
     def __init__(self):
@@ -23,7 +20,10 @@ class CloudtrailTester(interfaces.TesterInterface):
 
     def run_tests(self, args_object) -> list:
         return \
-        self.global_services_are_enabled()
+        self.global_services_are_enabled() + \
+        self.log_file_validation_is_enabled() + \
+        self.multi_region_is_enabled() + \
+        self.logs_encryption_is_enabled()
 
     def global_services_are_enabled(self):
         result = []
@@ -35,17 +35,89 @@ class CloudtrailTester(interfaces.TesterInterface):
             trailName = trail_meta["Name"]
             trailGlobal = trail_meta["IncludeGlobalServiceEvents"]
             trailBucket = trail_meta["S3BucketName"]
-            print (trailGlobal)  
             if  trailGlobal  == False:
                 result.append({
                     "item" : trailName,
                     "trail_bucket" : trailBucket,
-                    "test_name" : 'global_services_not_enabled',
+                    "test_name" : 'global_services_are_enabled',
                     "timestamp" : time.time()
                 })
         if len(result) == 0:
             result.append({
-            "test_name": 'global_services_not_enabled',
+            "test_name": 'global_services_are_enabled',
+            "item": None,
+            "timestamp": time.time()                    
+            })
+        return result
+    def log_file_validation_is_enabled(self):
+        result = []
+        #Get All Trails Description
+        trail_list = self.aws_cloudtrail_client.describe_trails(trailNameList=[],includeShadowTrails=False)
+        #Get the Array and process each element
+        trails_array = trail_list.get("trailList")
+        for trail_meta in trails_array:
+            trailName = trail_meta["Name"]
+            trailLogValidation = trail_meta["LogFileValidationEnabled"]
+            trailBucket = trail_meta["S3BucketName"]
+            if  trailLogValidation  == False:
+                result.append({
+                    "item" : trailName,
+                    "trail_bucket" : trailBucket,
+                    "test_name" : 'log_file_validation_is_enabled',
+                    "timestamp" : time.time()
+                })
+        if len(result) == 0:
+            result.append({
+            "test_name": 'log_file_validation_is_enabled',
+            "item": None,
+            "timestamp": time.time()                    
+            })
+        return result
+    
+    def multi_region_is_enabled(self):
+        result = []
+        #Get All Trails Description
+        trail_list = self.aws_cloudtrail_client.describe_trails(trailNameList=[],includeShadowTrails=False)
+        #Get the Array and process each element
+        trails_array = trail_list.get("trailList")
+        for trail_meta in trails_array:
+            trailName = trail_meta["Name"]
+            trailMultiRegion = trail_meta["IsMultiRegionTrail"]
+            trailBucket = trail_meta["S3BucketName"]
+            if  trailMultiRegion  == False:
+                result.append({
+                    "item" : trailName,
+                    "trail_bucket" : trailBucket,
+                    "test_name" : 'multi_region_is_enabled',
+                    "timestamp" : time.time()
+                })
+        if len(result) == 0:
+            result.append({
+            "test_name": 'multi_region_is_enabled',
+            "item": None,
+            "timestamp": time.time()                    
+            })
+        return result
+    
+    def logs_encryption_is_enabled(self):
+        result = []
+        #Get All Trails Description
+        trail_list = self.aws_cloudtrail_client.describe_trails(trailNameList=[],includeShadowTrails=False)
+        #Get the Array and process each element
+        trails_array = trail_list.get("trailList")
+        for trail_meta in trails_array:
+            trailName = trail_meta["Name"]
+            trailBucket = trail_meta["S3BucketName"]
+            if  ("KmsKeyId" in trail_meta)  == False:
+                result.append({
+                    "item" : trailName,
+                    "trail_bucket" : trailBucket,
+                    "test_name" : 'logs_encryption_is_enabled',
+                    "timestamp" : time.time()
+                })
+        if len(result) == 0:
+            result.append({
+            "test_name": 'logs_encryption_is_enabled',
             "item": None,
             "timestamp": time.time()                    
             })
