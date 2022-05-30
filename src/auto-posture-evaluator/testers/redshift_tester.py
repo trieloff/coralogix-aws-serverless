2ce54cee1f0e972eb1792eb3a7572eddf756dfc5
Sourav Mondal <souravcristiano502@gmail.com>
2022-05-30 16:49:47 +0530

    Add redshift cluster not encrypted with kms keys test (#80)
    * Add redshift cluster not encrypted with kms keys test
    
    * Add pagination to get all redshift clusters

--- src/auto-posture-evaluator/testers/redshift_tester.py
+++ src/auto-posture-evaluator/testers/redshift_tester.py
@@ -18,7 +18,7 @@ class Tester(interfaces.TesterInterface):
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
         self.account_arn = boto3.client('sts').get_caller_identity().get('Arn')
         self.account_id = boto3.client('sts').get_caller_identity().get('Account')
-        self.redshift_clusters = self.aws_redshift_client.describe_clusters()
+        self.redshift_clusters = self._get_all_redshift_clusters()
 
     def declare_tested_service(self) -> str:
         return 'redshift'
@@ -34,7 +34,8 @@ class Tester(interfaces.TesterInterface):
                self.detect_redshift_cluster_using_logging() + \
                self.detect_redshift_cluster_allow_version_upgrade() + \
                self.detect_redshift_cluster_requires_ssl() + \
-               self.detect_redshift_cluster_not_using_ec2_classic()
+               self.detect_redshift_cluster_not_using_ec2_classic() + \
+               self.get_redshift_cluster_not_encrypted_with_kms()
 
     def _append_redshift_test_result(self, redshift, test_name, issue_status):
         return {
@@ -155,3 +156,28 @@ class Tester(interfaces.TesterInterface):
                 result.append(self._append_redshift_test_result(redshift, test_name, "no_issue_found"))
         return result
 
+    def get_redshift_cluster_not_encrypted_with_kms(self):
+        test_name = "redshift_cluster_not_encrypted_with_KMS_customer_master_keys"
+        result = []
+
+        clusters = self.redshift_clusters["Clusters"]
+
+        for cluster in clusters:
+            encrypted = cluster["Encrypted"]
+
+            if encrypted:
+                result.append(self._append_redshift_test_result(cluster, test_name, "no_issue_found"))
+            else:
+                result.append(self._append_redshift_test_result(cluster, test_name, "issue_found"))
+            
+        return result
+
+    def _get_all_redshift_clusters(self):
+        clusters = []
+        paginator = self.aws_redshift_client.get_paginator('describe_clusters')
+        response_iterator = paginator.paginate()
+
+        for page in response_iterator:
+            clusters.extend(page['Clusters'])
+        
+        return { "Clusters" : clusters }
