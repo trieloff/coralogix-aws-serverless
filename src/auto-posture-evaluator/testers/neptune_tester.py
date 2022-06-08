36dec790d825b41e69daf246b9671ed7e662badb
Sourav Mondal <souravcristiano502@gmail.com>
2022-06-08 15:26:22 +0530

    Change testname according to the convention (#147)

--- src/auto-posture-evaluator/testers/neptune_tester.py
+++ src/auto-posture-evaluator/testers/neptune_tester.py
@@ -2,6 +2,7 @@ import time
 import boto3
 import interfaces
 
+
 class Tester(interfaces.TesterInterface):
     def __init__(self) -> None:
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
@@ -23,15 +24,15 @@ class Tester(interfaces.TesterInterface):
 
     def _get_all_neptune_clusters(self):
         db_clusters = []
-        
+
         paginator = self.aws_neptune_client.get_paginator('describe_db_clusters')
         response_iterator = paginator.paginate()
 
         for page in response_iterator:
             db_clusters.extend(page["DBClusters"])
-        
+
         return db_clusters
-    
+
     def _append_neptune_cluster_test_result(self, item, item_type, test_name, issue_status):
         return {
             "user": self.user_id,
@@ -43,10 +44,10 @@ class Tester(interfaces.TesterInterface):
             "test_name": test_name,
             "test_result": issue_status
         }
-    
+
     def get_database_encryption_disabled(self):
         result = []
-        test_name = "database_encryption_disabled"
+        test_name = "aws_neptune_database_encryption_disabled"
 
         db_clusters = self.db_clusters
 
@@ -63,14 +64,14 @@ class Tester(interfaces.TesterInterface):
 
     def get_neptune_cluster_audit_logs_disabled(self):
         result = []
-        test_name = "neptune_cluster_audit_logs_disabled"
+        test_name = "aws_neptune_cluster_audit_logs_disabled"
 
         db_clusters = self.db_clusters
 
         for instance in db_clusters:
             identifier = instance['DBClusterIdentifier']
             export_logs = instance.get('EnabledCloudwatchLogsExports')
-            
+
             if export_logs is not None:
                 if any([i.startswith("audit") for i in export_logs]):
                     result.append(self._append_neptune_cluster_test_result(identifier, "neptune_db_cluster", test_name, "no_issue_found"))
