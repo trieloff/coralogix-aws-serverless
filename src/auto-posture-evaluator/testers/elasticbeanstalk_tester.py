0f5e3cf48dfc17de463d4302e20177a8e25db94d
Sourav Mondal <souravcristiano502@gmail.com>
2022-06-09 14:33:15 +0530

    Change testname according to the convention (#150)

--- src/auto-posture-evaluator/testers/elasticbeanstalk_tester.py
+++ src/auto-posture-evaluator/testers/elasticbeanstalk_tester.py
@@ -1,8 +1,8 @@
 import time
-from traceback import print_tb
 import boto3
 import jmespath
 import interfaces
+
 
 class Tester(interfaces.TesterInterface):
     def __init__(self) -> None:
@@ -14,10 +14,10 @@ class Tester(interfaces.TesterInterface):
 
     def declare_tested_provider(self) -> str:
         return "aws"
-    
+
     def declare_tested_service(self) -> str:
         return "elasticbeanstalk"
-    
+
     def run_tests(self) -> list:
         return \
             self.application_environment_should_have_load_balancer_access_logs() + \
@@ -44,12 +44,12 @@ class Tester(interfaces.TesterInterface):
 
         for page in response_iterator:
             environments.extend(page['Environments'])
-        
+
         return environments
 
     def application_environment_should_have_load_balancer_access_logs(self):
         result = []
-        test_name = "elasticbeanstalk_application_environment_should_have_load_balancer_access_logs"
+        test_name = "aws_elasticbeanstalk_application_environment_should_have_load_balancer_access_logs"
         environments = self.elasticbeanstalk_enviroments
 
         for env in environments:
@@ -61,21 +61,22 @@ class Tester(interfaces.TesterInterface):
             filtered_response = jmespath.search("configuration_settings[*].OptionSettings[?(OptionName==`AccessLogsS3Enabled`)].Value | []", configuration_settings)
 
             access_logs_enabled = filtered_response
-            if not access_logs_enabled: 
+            if not access_logs_enabled:
                 result.append(self._append_elasticbeanstalk_test_result(environment_name, "elasticbeanstalk_application_environment", test_name, "issue_found"))
             elif access_logs_enabled[0] == "false":
                 result.append(self._append_elasticbeanstalk_test_result(environment_name, "elasticbeanstalk_application_environment", test_name, "issue_found"))
             else:
                 result.append(self._append_elasticbeanstalk_test_result(environment_name, "elasticbeanstalk_application_environment", test_name, "no_issue_found"))
-        
+
         return result
 
     def enhanced_health_enabled(self):
         result = []
-        test_name = "enhanced_health_reporting_enabled"
+        test_name = "aws_elasticbeanstalk_enhanced_health_reporting_enabled"
         environments = self.elasticbeanstalk_enviroments
-        
-        filtered_environments = list(filter(lambda env: env['Status'] == "Ready" or env['Status'] == "Updating" or env["Status"] == "Launching" or env["Status"] == "LinkingFrom" or env["Status"] == "LinkingTo", environments))
+
+        filtered_environments = list(filter(lambda env: env['Status'] == "Ready" or env['Status'] == "Updating" or env["Status"]
+                                     == "Launching" or env["Status"] == "LinkingFrom" or env["Status"] == "LinkingTo", environments))
 
         for env in filtered_environments:
             env_name = env['EnvironmentName']
@@ -85,42 +86,42 @@ class Tester(interfaces.TesterInterface):
                 result.append(self._append_elasticbeanstalk_test_result(env_name, "elasticbeanstalk_application_environment", test_name, "no_issue_found"))
             else:
                 result.append(self._append_elasticbeanstalk_test_result(env_name, "elasticbeanstalk_application_environment", test_name, "issue_found"))
-        
+
         return result
 
     def application_env_has_managed_updates_enabled(self):
         result = []
-        test_name = "application_environment_should_have_managed_platform_updates_enabled"
+        test_name = "aws_elasticbeanstalk_application_environment_should_have_managed_platform_updates_enabled"
         environments = self.elasticbeanstalk_enviroments
-        
+
         for env in environments:
             env_name = env['EnvironmentName']
             app_name = env['ApplicationName']
 
             response = self.aws_elasticbeanstalk_client.describe_configuration_settings(ApplicationName=app_name, EnvironmentName=env_name)
-            configuration_settings = {"ConfigurationSettings" : response["ConfigurationSettings"]}
+            configuration_settings = {"ConfigurationSettings": response["ConfigurationSettings"]}
             temp = jmespath.search("ConfigurationSettings[*].OptionSettings[?OptionName==`ManagedActionsEnabled`] | []", configuration_settings)
             if len(temp) > 0:
                 managed_actions = temp[0]['Value']
                 if managed_actions == "true":
                     result.append(self._append_elasticbeanstalk_test_result(env_name, "elasticbeanstalk_application_environment", test_name, "no_issue_found"))
-                else: 
+                else:
                     result.append(self._append_elasticbeanstalk_test_result(env_name, "elasticbeanstalk_application_environment", test_name, "issue_found"))
             else: pass
-        
+
         return result
 
     def detect_environment_notification_configured(self):
         result = []
-        test_name = "environment_notifications_should_be_configured"
+        test_name = "aws_elasticbeanstalk_environment_notifications_should_be_configured"
         environments = self.elasticbeanstalk_enviroments
-        
+
         for env in environments:
             env_name = env['EnvironmentName']
             app_name = env['ApplicationName']
 
             response = self.aws_elasticbeanstalk_client.describe_configuration_settings(ApplicationName=app_name, EnvironmentName=env_name)
-            configuration_settings = {"ConfigurationSettings" : response["ConfigurationSettings"]}
+            configuration_settings = {"ConfigurationSettings": response["ConfigurationSettings"]}
             temp = jmespath.search("ConfigurationSettings[*].OptionSettings[?OptionName==`Notification Endpoint`].Value | []", configuration_settings)
 
             if not temp:
