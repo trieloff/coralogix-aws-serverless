283d5f17ad048313dfe537c2dfb6bd7159d59a1e
Bhuvanesh-J <87064334+Bhuvanesh-J@users.noreply.github.com>
2022-06-16 16:30:13 +0530

    Region name and multithreading feature added for EBS Tester (#171)

--- src/auto-posture-evaluator/testers/ebs_tester.py
+++ src/auto-posture-evaluator/testers/ebs_tester.py
@@ -6,12 +6,13 @@ import interfaces
 import datetime as dt
 from datetime import datetime
 import os
+import concurrent.futures
 
 class Tester(interfaces.TesterInterface):
-    def __init__(self) -> None:
-        self.aws_ec2_client = boto3.client('ec2')
-        self.aws_ec2_resource = boto3.resource('ec2')
-        self.aws_kms_client = boto3.client('kms')
+    def __init__(self, region_name) -> None:
+        self.aws_ec2_client = boto3.client('ec2', region_name=region_name)
+        self.aws_ec2_resource = boto3.resource('ec2', region_name=region_name)
+        self.aws_kms_client = boto3.client('kms', region_name=region_name)
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
         self.account_arn = boto3.client('sts').get_caller_identity().get('Arn')
         self.account_id = boto3.client('sts').get_caller_identity().get('Account')
@@ -25,12 +26,18 @@ class Tester(interfaces.TesterInterface):
 
     def run_tests(self) -> list:
         self.ebs_volumes = self._get_ebs_volumes()
-        return \
-            self.get_volume_is_not_encrypted(self.ebs_volumes) + \
-            self.get_volume_attached_to_ec2(self.ebs_volumes) + \
-            self.get_volume_does_not_have_recent_snapshots(self.ebs_volumes) + \
-            self.get_volume_not_encrypted_with_kms_customer_keys(self.ebs_volumes) + \
-            self.get_volume_snapshots_are_public()
+        executor_list = []
+        return_value = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor_list.append(executor.submit(self.get_volume_is_not_encrypted, self.ebs_volumes))
+            executor_list.append(executor.submit(self.get_volume_attached_to_ec2, self.ebs_volumes))
+            executor_list.append(executor.submit(self.get_volume_does_not_have_recent_snapshots, self.ebs_volumes))
+            executor_list.append(
+                executor.submit(self.get_volume_not_encrypted_with_kms_customer_keys, self.ebs_volumes))
+            executor_list.append(executor.submit(self.get_volume_snapshots_are_public))
+            for future in executor_list:
+                return_value += future.result()
+        return return_value
 
     def _get_ebs_volumes(self):
         volumes = []
@@ -119,11 +126,13 @@ class Tester(interfaces.TesterInterface):
             can_paginate_snapshot = self.aws_ec2_client.can_paginate('describe_snapshots')
             if can_paginate_snapshot:
                 paginator_snap = self.aws_ec2_client.get_paginator('describe_snapshots')
-                snap_response_iterator = paginator_snap.paginate(PaginationConfig={'PageSize': 50}, Filters=[{'Name': 'volume-id', 'Values': [volume_id]}])
+                snap_response_iterator = paginator_snap.paginate(PaginationConfig={'PageSize': 50},
+                                                                 Filters=[{'Name': 'volume-id', 'Values': [volume_id]}])
                 for page in snap_response_iterator:
                     snapshots.extend(page['Snapshots'])
             else:
-                snap_response = self.aws_ec2_client.describe_snapshots(Filters=[{'Name': 'volume-id', 'Values': [volume_id]}])
+                snap_response = self.aws_ec2_client.describe_snapshots(
+                    Filters=[{'Name': 'volume-id', 'Values': [volume_id]}])
                 snapshots.extend(snap_response['Snapshots'])
             recent_snapshot_found = False
             for snapshot in snapshots:
@@ -131,7 +140,7 @@ class Tester(interfaces.TesterInterface):
                 create_date = snapshot['StartTime']
                 current_date = datetime.now(tz=dt.timezone.utc)
                 time_diff = (current_date - create_date).days
-                if time_diff<os.environ.get('THRESHOLD', 7):
+                if time_diff < os.environ.get('THRESHOLD', 7):
                     recent_snapshot_found = True
                     break
             if recent_snapshot_found:
@@ -210,12 +219,14 @@ class Tester(interfaces.TesterInterface):
     def get_volume_snapshots_are_public(self):
         test_name = "aws_ebs_volume_snapshots_are_public"
         result = []
-        snapshots = self.aws_ec2_client.describe_snapshots(OwnerIds=[self.account_id], Filters=[{"Name": "status", "Values":["completed"]}])
+        snapshots = self.aws_ec2_client.describe_snapshots(OwnerIds=[self.account_id],
+                                                           Filters=[{"Name": "status", "Values": ["completed"]}])
 
         for snapshot in snapshots["Snapshots"]:
             snapshot_id = snapshot["SnapshotId"]
-            attrs = self.aws_ec2_client.describe_snapshot_attribute(SnapshotId=snapshot_id, Attribute="createVolumePermission")
-            if any([attr["Group"]=="all" for attr in attrs["CreateVolumePermissions"]]):
+            attrs = self.aws_ec2_client.describe_snapshot_attribute(SnapshotId=snapshot_id,
+                                                                    Attribute="createVolumePermission")
+            if any([attr["Group"] == "all" for attr in attrs["CreateVolumePermissions"]]):
                 result.append({
                     "user": self.user_id,
                     "account_arn": self.account_arn,
