1cbb805d1554dcb0f1d051bc4316d7868ba92800
Sourav Mondal <souravcristiano502@gmail.com>
2022-06-19 16:58:01 +0530

    Add region and multi-thread executor in Lambda tester (#178)
    * Add region in lambda tester
    
    * Add multi thread executor

--- src/auto-posture-evaluator/testers/lambda_tester.py
+++ src/auto-posture-evaluator/testers/lambda_tester.py
@@ -5,11 +5,12 @@ import re
 import interfaces
 import boto3
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 
 class Tester(interfaces.TesterInterface):
-    def __init__(self) -> None:
-        self.aws_lambda_client = boto3.client('lambda')
+    def __init__(self, region_name) -> None:
+        self.aws_lambda_client = boto3.client('lambda', region_name=region_name)
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
         self.account_arn = boto3.client('sts').get_caller_identity().get('Arn')
         self.account_id = boto3.client('sts').get_caller_identity().get('Account')
@@ -23,10 +24,18 @@ class Tester(interfaces.TesterInterface):
         return 'aws'
 
     def run_tests(self) -> list:
-        return \
-            self.get_lambda_publicly_accessible() + \
-            self.get_lambda_has_access_to_vpc_resources() + \
-            self.get_lambda_uses_latest_runtime()
+        executor_list = []
+        return_values = []
+
+        with ThreadPoolExecutor() as executor:
+            executor_list.append(executor.submit(self.get_lambda_publicly_accessible))
+            executor_list.append(executor.submit(self.get_lambda_has_access_to_vpc_resources))
+            executor_list.append(executor.submit(self.get_lambda_uses_latest_runtime))
+
+            for future in executor_list:
+                return_values.extend(future.result())
+
+        return return_values
 
     def _get_all_functions(self) -> List:
         paginator = self.aws_lambda_client.get_paginator('list_functions')
