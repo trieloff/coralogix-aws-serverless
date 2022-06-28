ba950cf76b438055f9b9f78beed2a9ef22bcb88e
Bhuvanesh-J <87064334+Bhuvanesh-J@users.noreply.github.com>
2022-06-28 13:01:00 +0530

    For RDS Tester region key added to result and region filter added (#203)

--- src/auto-posture-evaluator/testers/rds_tester.py
+++ src/auto-posture-evaluator/testers/rds_tester.py
@@ -19,13 +19,15 @@ def _return_default_port_on_rds_engines(db_engine):
 
 class Tester(interfaces.TesterInterface):
     def __init__(self, region_name):
+        self.ssm = boto3.client('ssm')
+        self.region_name = region_name
         self.aws_rds_client = boto3.client('rds', region_name=region_name)
         self.cache = {}
         self.user_id = boto3.client('sts').get_caller_identity().get('UserId')
         self.account_arn = boto3.client('sts').get_caller_identity().get('Arn')
         self.account_id = boto3.client('sts').get_caller_identity().get('Account')
-        self.rds_instances = self.aws_rds_client.describe_db_instances()
-        self.rds_snapshots = self.aws_rds_client.describe_db_snapshots()
+        self.rds_instances = []
+        self.rds_snapshots = []
 
     def declare_tested_service(self) -> str:
         return 'rds'
@@ -34,6 +36,11 @@ class Tester(interfaces.TesterInterface):
         return 'aws'
 
     def run_tests(self) -> list:
+        if self.region_name == 'global' or self.region_name not in self._get_regions():
+            return None
+        self.rds_instances = self.aws_rds_client.describe_db_instances()
+        self.rds_snapshots = self.aws_rds_client.describe_db_snapshots()
+
         executor_list = []
         return_value = []
         with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -54,6 +61,15 @@ class Tester(interfaces.TesterInterface):
                 return_value += future.result()
         return return_value
 
+    def _get_regions(self) -> list:
+        region_list = []
+        for page in self.ssm.get_paginator('get_parameters_by_path').paginate(
+                Path='/aws/service/global-infrastructure/regions'
+        ):
+            for p in page['Parameters']:
+                region_list.append(p['Value'])
+        return region_list
+
     def _append_rds_test_result(self, rds, test_name, issue_status):
         return {
             "user": self.user_id,
@@ -63,7 +79,8 @@ class Tester(interfaces.TesterInterface):
             "item": rds['DBInstanceIdentifier'],
             "item_type": "rds_db_instance",
             "test_name": test_name,
-            "test_result": issue_status
+            "test_result": issue_status,
+            "region": self.region_name
         }
 
     def _append_rds_snap_test_result(self, rds, test_name, issue_status):
@@ -75,7 +92,8 @@ class Tester(interfaces.TesterInterface):
             "item": rds['DBSnapshotIdentifier'],
             "item_type": "rds_snapshot",
             "test_name": test_name,
-            "test_result": issue_status
+            "test_result": issue_status,
+            "region": self.region_name
         }
 
     def _fetch_snapshot_metadata(self, snapshot_identifier):
