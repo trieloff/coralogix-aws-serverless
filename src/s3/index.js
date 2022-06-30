8645583734a4f0a151d0753b719d3e57e221a5b3
Lars Trieloff <lars@trieloff.net>
2022-06-30 16:56:27 +0200

    feat(s3): transform from cloudflare log format into fastly log format

--- src/s3/index.js
+++ src/s3/index.js
@@ -19,6 +19,88 @@ const assert = require("assert");
 const coralogix = require("coralogix-logger");
 const s3 = new aws.S3();
 
+
+const exampleinput = {
+    "ZoneID": 549103045,
+    "ClientMTLSAuthStatus": "unknown",
+    "OriginResponseTime": 0,
+    "OriginTLSHandshakeDurationMs": 0,
+    "ZoneName": "hlx.page",
+    "OriginResponseHTTPExpires": "",
+    "RayID": "722716067dff9112",
+    "SecurityLevel": "med",
+    "EdgeResponseCompressionRatio": 3.61,
+    "RequestHeaders": {
+        "accept": "*\/*",
+        "accept-encoding": "gzip, deflate",
+        "connection": "keep-alive"
+    },
+    "ClientRequestScheme": "https",
+    "OriginRequestHeaderSendDurationMs": 0,
+    "EdgeColoID": 472,
+    "ClientRequestProtocol": "HTTP\/1.1",
+    "UpperTierColoID": 0,
+    "ClientSrcPort": 63378,
+    "EdgeResponseStatus": 200,
+    "OriginResponseHTTPLastModified": "",
+    "OriginResponseBytes": 0,
+    "ClientSSLProtocol": "TLSv1.3",
+    "EdgePathingOp": "wl",
+    "ResponseHeaders": {
+        "last-modified": "Wed, 25 May 2022 12:38:15 GMT",
+        "vary": "Accept-Encoding"
+    },
+    "ClientIP": "130.248.57.10",
+    "CacheTieredFill": false,
+    "ClientTCPRTTMs": 12,
+    "ClientMTLSAuthCertFingerprint": "",
+    "ClientSSLCipher": "AEAD-AES256-GCM-SHA384",
+    "ParentRayID": "00",
+    "OriginTCPHandshakeDurationMs": 0,
+    "EdgeRequestHost": "cloufflare--helix-website--adobe.hlx.page",
+    "ClientRequestUserAgent": "HTTPie\/3.2.1",
+    "EdgePathingSrc": "macro",
+    "OriginResponseHeaderReceiveDurationMs": 0,
+    "EdgeCFConnectingO2O": false,
+    "EdgeRateLimitID": 0,
+    "ClientDeviceType": "desktop",
+    "ClientIPClass": "noRecord",
+    "WorkerSubrequestCount": 0,
+    "OriginSSLProtocol": "unknown",
+    "EdgeResponseBodyBytes": 863,
+    "EdgeRateLimitAction": "",
+    "EdgeResponseBytes": 1893,
+    "ClientRequestURI": "\/",
+    "WorkerSubrequest": false,
+    "EdgeStartTimestamp": "2022-06-28T14:24:26Z",
+    "EdgeTimeToFirstByteMs": 506,
+    "ClientRequestHost": "cloufflare--helix-website--adobe.hlx.page",
+    "ClientRequestPath": "\/",
+    "WorkerStatus": "ok",
+    "OriginResponseStatus": 0,
+    "CacheCacheStatus": "unknown",
+    "OriginIP": "",
+    "ClientASN": 44786,
+    "ClientCountry": "de",
+    "ClientRequestReferer": "",
+    "OriginResponseDurationMs": 0,
+    "ClientRequestBytes": 3114,
+    "EdgeResponseContentType": "text\/html",
+    "WorkerCPUTime": 105335,
+    "SmartRouteColoID": 0,
+    "EdgeColoCode": "FRA",
+    "EdgeServerIP": "",
+    "OriginDNSResponseTimeMs": 0,
+    "EdgePathingStatus": "nr",
+    "CacheResponseBytes": 3734,
+    "ClientXRequestedWith": "",
+    "ClientRequestMethod": "GET",
+    "ClientRequestSource": "eyeball",
+    "CacheResponseStatus": 200,
+    "EdgeEndTimestamp": "2022-06-28T14:24:27Z"
+};
+console.log(cloudflare2fastly(exampleinput));
+
 // Check Lambda function parameters
 assert(process.env.private_key, "No private key!");
 const newlinePattern = process.env.newline_pattern ? RegExp(process.env.newline_pattern) : /(?:\r\n|\r|\n)/g;
@@ -50,18 +132,88 @@ function sendLogs(content, filename) {
         try {
             appName = appName.startsWith("$.") ? dig(appName, JSON.parse(logs[i])) : appName;
             subName = subName.startsWith("$.") ? dig(subName, JSON.parse(logs[i])) : subName;
-        } catch {}
+        } catch { }
 
-        logger.addLog(
-            appName,
-            subName,
-            new coralogix.Log({
-                severity: getSeverityLevel(logs[i]),
-                text: logs[i],
-                threadId: filename
-            })
-        );
+        try {
+            const cloudflarejson = JSON.parse(logs[i]);
+            subName = cloudflarejson.ZoneName;
+
+            const fastlyjson = cloudflare2fastly(cloudflarejson);
+            logger.addLog(
+                appName,
+                subName,
+                new coralogix.Log({
+                    severity: getSeverityLevelFromStatusCode(cloudflarejson.EdgeResponseStatus),
+                    text: JSON.stringify(fastlyjson),
+                    threadId: cloudflarejson.RayID,
+                })
+            );
+        } catch {
+            logger.addLog(
+                appName,
+                subName,
+                new coralogix.Log({
+                    severity: getSeverityLevel(logs[i]),
+                    text: logs[i],
+                    threadId: filename
+                })
+            );
+        }
+
     }
+}
+/**
+ * Transforms Cloudflare JSON log messages to Fastly JSON log messages
+ * @param {object} cloudflarejson 
+ * @returns {object} Fastly JSON log message
+ */
+function cloudflare2fastly(cloudflarejson) {
+    return {
+        request: {
+            method: cloudflarejson.ClientRequestMethod, // ClientRequestMethod
+            host: cloudflarejson.ClientRequestHost, // ClientRequestHost
+            url: cloudflarejson.ClientRequestURI, // ClientRequestURI
+            protocol: cloudflarejson.ClientRequestProtocol, // ClientRequestProtocol
+            header_size: cloudflarejson.ClientRequestBytes, // ClientRequestBytes
+            headers: Object.entries(cloudflarejson.RequestHeaders).reduce((acc, [key, value]) => {
+                acc[key.toLowerCase().replace(/-/g, '_')] = value;
+                return acc;
+            }, {}), // RequestHeaders
+        },
+        response: {
+            status: cloudflarejson.EdgeResponseStatus, // EdgeResponseStatus
+            header_size: cloudflarejson.EdgeResponseBytes - cloudflarejson.EdgeResponseBodyBytes, // EdgeResponseBytes
+            body_size: cloudflarejson.EdgeResponseBodyBytes, // EdgeResponseBodyBytes
+            headers: Object.entries(cloudflarejson.ResponseHeaders).reduce((acc, [key, value]) => {
+                acc[key.toLowerCase().replace(/-/g, '_')] = value;
+                return acc;
+            }, {
+                "content-type": cloudflarejson.EdgeResponseContentType, // EdgeResponseContentType
+            }), // RequestHeaders
+        },
+        client: {
+            number: cloudflarejson.ClientASN, // ClientASN
+            ip: cloudflarejson.ClientIP, // ClientIP
+        },
+        cdn: {
+            service_id: cloudflarejson.ZoneID, // ZoneID
+            url: new URL(cloudflarejson.ClientRequestURI, cloudflarejson.ClientRequestScheme + "://" + cloudflarejson.ClientRequestHost).href,
+            originating_ip: cloudflarejson.RequestHeaders['x-forwarded-for'] ? cloudflarejson.RequestHeaders['x-forwarded-for'].split(',')[0] : cloudflarejson.ClientIP,
+            time: {
+                start: cloudflarejson.EdgeStartTimestamp, // EdgeStartTimestamp
+                start_msec: new Date(cloudflarejson.EdgeStartTimestamp).getTime(),
+                end: cloudflarejson.EdgeEndTimestamp, // EdgeEndTimestamp
+                end_msec: new Date(cloudflarejson.EdgeEndTimestamp).getTime(),
+                elapsed: cloudflarejson.EdgeTimeToFirstByteMs,
+                worker_cpu_time: cloudflarejson.WorkerCPUTime,
+            },
+            is_edge: cloudflarejson.ClientRequestSource === 'eyeball',
+            datacenter: cloudflarejson.EdgeColoCode, // EdgeColoCode
+            hostname: "cloudflare-" + cloudflarejson.EdgeColoCode.toLowerCase() + "-" + cloudflarejson.EdgeColoID,
+            ip: cloudflarejson.EdgeServerIP, // EdgeServerIP
+            cache_status: cloudflarejson.CacheCacheStatus, // CacheCacheStatus
+        }
+    };
 }
 
 /**
@@ -96,6 +248,15 @@ function getSeverityLevel(message) {
         severity = 5;
     if (message.includes("critical") || message.includes("panic"))
         severity = 6;
+    return severity;
+}
+
+function getSeverityLevelFromStatusCode(code) {
+    let severity = 3;
+    if (code >= 400 && code < 500)
+        severity = 4;
+    if (code >= 500 && code < 600)
+        severity = 5;
     return severity;
 }
 
